94fef6b68beb1b982b55de24fdecb62d12d01b7d
nitinchandekar <chand.nitin333@gmail.com>
2024-12-29 15:53:31 +0530

    Login page outside

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
+import { Router, RouterModule } from '@angular/router';
 import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from '../header/header.component';
 import { LoaderComponent } from '../loader/loader.component';
+import { LoginComponent } from '../login/login.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 @Component({
   selector: 'app-layout',
   standalone: true,
@@ -13,10 +14,18 @@ import { LoaderComponent } from '../loader/loader.component';
     RouterModule,
     MatSidenavModule,
     HeaderComponent,
-    SidebarComponent, FooterComponent, LoaderComponent],
+    SidebarComponent, FooterComponent, LoaderComponent, LoginComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  isLoginPage: boolean = false;
+  constructor(private router: Router) {
+
+    this.router.events.subscribe(() => {
+      // Check if the current route is 'login'
+      this.isLoginPage = this.router.url === '/login';
+    });
+  }
 
 }
