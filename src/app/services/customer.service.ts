02bfb54f120658d8ed96985b9482c467443c1671
Kundan Kotangale <kundanmkotangale@gmail.com>
2025-03-29 20:34:32 +0530

    update customer list module

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerService {
-
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService) {}
 
   fetchCustomersList(params: any) {
     return this.api.post('get-malmatta-nodni-list-info', params);
@@ -24,19 +23,29 @@ export class CustomerService {
     return this.api.post(`verify-user-for-permission`, params);
   }
 
-
-
-
-
-
-
-
-  updatePrakar(params: any) {
-    return this.api.put(`update-prakar`, params);
+  getMalmattaNodniById(id: number) {
+    return this.api.get(`get-malmatta-nodni-user-by-id/${id}`);
   }
-  deletePrakar(id: number) {
-    return this.api.delete(`delete-prakar/${id}`);
+
+  updateCustomerList(params: any) {
+    return this.api.put(`update-malmatta-nodni`, params);
   }
+  getDropdownYearsList() {
+    return this.api.get(`get-all-year-list`);
+  }
+
+  addSillakJoda(params: any) {
+    return this.api.post(`insert-update-sillak-joda`, params);
+  }
+  deleteCustomerFromNodani(id: number) {
+    return this.api.delete(`delete-malmatta-nodni-info/${id}`);
+  }
+
+
+
+
+
+
 
   getPrakarById(id: number) {
     return this.api.get(`prakar/${id}`);
