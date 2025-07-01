b2f1ae5c89e300b24d85b48ef3ed009135a990d7
Kundan Kotangale <kundanmkotangale@gmail.com>
2025-07-01 11:34:30 +0530

    update url

--- src/constant/admin.constant.ts
+++ src/constant/admin.constant.ts
@@ -1,6 +1,6 @@
 
-// export const API_URL = 'http://103.102.234.151:4444/api/';
-export const API_URL = 'http://localhost:4444/api/';
+export const API_URL = 'http://103.102.234.151:4444/api/';
+// export const API_URL = 'http://localhost:4444/api/';
 
 export const API_KEY = 'AIzaSyCsQc4fLXZfZcSU00oHFdGWOZG4qiq7af8';
 export const TRANSLATE_API_URL = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
