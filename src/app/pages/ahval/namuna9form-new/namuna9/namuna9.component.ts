5333bfa3d2e240aca754231c2810ee73205e0b09
Kundan Kotangale <kundanmkotangale@gmail.com>
2026-01-03 21:58:46 +0530

    update 5 percent less and plus

--- src/app/pages/ahval/namuna9form-new/namuna9/namuna9.component.ts
+++ src/app/pages/ahval/namuna9form-new/namuna9/namuna9.component.ts
@@ -14,6 +14,7 @@ import { LoaderService } from '../../../../services/loader.service';
   styleUrl: './namuna9.component.css'
 })
 export class Namuna9Component {
+    Math = Math;
     receivedData : any;
     reportData: any;
     ward_number: any;
