83271763e2170b7f69359c3ffe71da81feaac66e
Kundan Kotangale <kundanmkotangale@gmail.com>
2026-06-06 19:23:13 +0530

    update

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,8 +8,8 @@ import { catchError, Observable, throwError } from 'rxjs';
 })
 export class ApiService {
 
-  private baseUrl: string = 'http://gramvikas.co.in/api';
-  public file_baseUrl: string = 'http://gramvikas.co.in/uploads/';
+  private baseUrl: string = 'https://gramvikas.co.in/api';
+  public file_baseUrl: string = 'https://gramvikas.co.in/uploads/';
   // public file_baseUrl: string = 'http://localhost:4444/uploads/';
   // public  baseUrl: string = 'http://localhost:4444/api';
   constructor(private http: HttpClient, private router: Router) {
