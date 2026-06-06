38f81772a4a0e07f09996895ed3facd24d45d3df
chandnitin333 <chand.nitin333@gmail.com>
2026-06-06 20:45:35 +0530

    config Update

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { catchError, Observable, throwError } from 'rxjs';
 })
 export class ApiService {
 
-  private baseUrl: string = 'http://103.102.234.151:4444/api';
+  private baseUrl: string = 'https://gramvikas.co.in/api';
   public file_baseUrl: string = 'http://103.102.234.151:4444/uploads/';
   // public file_baseUrl: string = 'http://localhost:4444/uploads/';
   // public  baseUrl: string = 'http://localhost:4444/api';
