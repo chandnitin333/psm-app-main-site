60bb62ac2cde60094fa168a771055b7ad959b4fc
Kundan Kotangale <kundanmkotangale@gmail.com>
2026-06-24 22:58:32 +0530

    update url

--- src/constant/admin.constant.ts
+++ src/constant/admin.constant.ts
@@ -1,7 +1,7 @@
 
 // export const API_URL = 'https://gramvikas.co.in/api';
 // export const API_URL = 'http://localhost:4444/api';
-export const API_URL = 'http://103.102.234.151:4444/api';
+export const API_URL = ' https://oldapi.gramvikas.co.in/api';
 
 export const API_KEY = 'AIzaSyCsQc4fLXZfZcSU00oHFdGWOZG4qiq7af8';
 export const TRANSLATE_API_URL = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
